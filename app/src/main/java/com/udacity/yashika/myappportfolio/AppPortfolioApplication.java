72481a6132c70c5f61b938df1b1ae291ab46239c
YashikaKCopper <yashika.k@coppermobile.com>
2015-12-10 21:04:55 +0530

    dependencies

---
+++ app/src/main/java/com/udacity/yashika/myappportfolio/AppPortfolioApplication.java
@@ -0,0 +1,11 @@
+package com.udacity.yashika.myappportfolio;
+
+import android.app.Application;
+
+/**
+ * @author yashika.
+ */
+public class AppPortfolioApplication extends Application {
+
+    private static final String MOVIE_API_KEY = "2a558c0aa900e0db7e502f10595635e9";
+}
